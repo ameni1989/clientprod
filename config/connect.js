7ecd13e5cf25b2770f3af85f7c1fe0936908d885
ameni1989 <147339046+ameni1989@users.noreply.github.com>
2024-05-26 03:30:33 +0100

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 mongoose.connect('mongodb+srv://ameniweslati889:pass_word@cluster0.2qnbyfq.mongodb.net/ecole_formation?retryWrites=true&w=majority&appName=Cluster0')
 .then (
     ()=>{
-        console.log('connected');
+        console.log('connected to mongodb');
     }
 )
 .catch (
