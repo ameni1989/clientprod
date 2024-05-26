93af980f456b953e58268e5928a009d65cbf2a21
ameni1989 <147339046+ameni1989@users.noreply.github.com>
2024-05-26 15:59:40 +0100

    Update connect.js

--- config/connect.js
+++ config/connect.js
@@ -1,26 +1,14 @@
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://ameniweslati889:KIPrEyIcFL9HL3oR@clusterm.tgvzaw8.mongodb.net/nodetest?retryWrites=true&w=majority&appName=ClusterM')
-.then (
-    ()=>{
-        console.log('connected to mongodb');
-    }
-)
-.catch (
-    (err)=>{
-        console.log(err);
-    }
-)
-module.exports=mongoose;
-')
-.then (
-    ()=>{
-        console.log('connected to mongodb');
-    }
-)
-.catch (
-    (err)=>{
-        console.log(err);
-    }
-)
-module.exports=mongoose;
+mongoose
+  .connect(
+    "mongodb+srv://ameniweslati889:KIPrEyIcFL9HL3oR@clusterm.tgvzaw8.mongodb.net/nodetest?retryWrites=true&w=majority&appName=ClusterM"
+  )
+  .then(() => {
+    console.log("connected to mongodb hh");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+module.exports = mongoose;
